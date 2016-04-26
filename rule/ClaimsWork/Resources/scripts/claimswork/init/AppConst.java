c20bb5a2222c8b8044c9e890a10cf93e61d44eec
k-zon_000 <k-zon_000@KayraNote>
2016-04-26 11:19:32 +0300

    refactor

--- rule/ClaimsWork/Resources/scripts/claimswork/init/AppConst.java
+++ rule/ClaimsWork/Resources/scripts/claimswork/init/AppConst.java
@@ -4,7 +4,7 @@ public class AppConst {
 	public static String NAME = "ClaimsWork";
 	public static String NAME_ENG = "ClaimsWork";
 	public static String NAME_RUS = "Претензионно-исковая работа";
-	public static String NAME_KZ = "Шағымдар жұмыс";
+	public static String NAME_KAZ = "Шағымдар жұмыс";
 	public static String DEFAULT_URL = "p?id=claim-view";
 	public static String FT_INDEX_SCOPE = "";
 }
