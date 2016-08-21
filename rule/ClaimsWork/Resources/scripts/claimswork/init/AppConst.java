f82b006185207ffbd3b36e4cb95909b2d942742e
k-zon_000 <k-zon_000@KayraNote>
2016-08-21 15:52:02 +0300

    #

--- rule/ClaimsWork/Resources/scripts/claimswork/init/AppConst.java
+++ rule/ClaimsWork/Resources/scripts/claimswork/init/AppConst.java
@@ -1,10 +1,11 @@
 package claimswork.init;
 
 public class AppConst {
+	public static String MODULE_VERSION = "1.0";
 	public static String NAME = "ClaimsWork";
 	public static String NAME_ENG = "ClaimsWork";
 	public static String NAME_RUS = "Претензионно-исковая работа";
 	public static String NAME_KAZ = "Шағымдар жұмыс";
-	public static String DEFAULT_URL = "p?id=claim-view";
+	public static String DEFAULT_PAGE = "claim-view";
 	public static String FT_INDEX_SCOPE = "";
 }
